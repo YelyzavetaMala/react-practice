7781b22af6a20ee1671cab8a7b387d6b915ba524
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-02-29 21:39:24 +0200

    3

--- src/App.jsx
+++ src/App.jsx
@@ -39,22 +39,39 @@ const ProductData = [
     description: " Tacos are a beloved Mexican dish enjoyed worldwide for their versatility and delicious flavors.These handheld treats typically consist of a corn or flour tortilla filled with a variety of ingredients such as seasoned meat, vegetables, cheese, and salsa",
 }]
 
+const emailsData = [
+  { id: "1", email: "foo@bar.com" },
+  { id: "2", email: "alex@bar.com" },
+  { id: "3", email: "im0986@gmail.com"}
+]
 
 function App() {
   const [counter, setCounter] = useState(0);
-  
+  const [emails, setEmails] = useState(emailsData);
+  const [showMailBox, setshowMailBox] = userState(false);
 
   const onLogEmail = () => {
     console.log("Email was sent");
-    setCounter(counter + 1)
+    setCounter(prevState => prevState + 1);
   }
   const handleDelete = (mailId) => {
-        console.log("mailId: ", mailId);
+    console.log("mailId: ", mailId);
+    setEmails(prevState => prevState.filter(email => email.id !== mailId));
     };
 
+  const handleToggleMailBox = () => {
+    setshowMailBox(prevState => !prevState)
+  }
+  
   return <div>
     <h1>Email Counter: </h1>
-    <MailBox onLogEmail={onLogEmail} onDeleteEmail={handleDelete} />
+    <button onClick={handleToggleMailBox}>Show MailBox</button>
+    {showMailBox ? <MailBox
+      emails={emails}
+      emailCounter={counter}
+      onLogEmail={onLogEmail}
+      onDeleteEmail={handleDelete}
+    /> : null}
 
     {ProductData.map(item => {
       const isPromotional = item.quantity <= 2;
